bf6a4fd430e549077dfb0cfbdd64ac8bc623c37c
Jeremy Eudy <jeremyeudy@gmail.com>
2018-06-19 15:26:08 -0400

    Modified for discord.py 1.0.0a0

--- Dungeon-Master.py
+++ Dungeon-Master.py
@@ -1,7 +1,7 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                             |\               #
-#    StrawPollBot.py                                    ------| \----          #
+#    Dungeon-Master.py                                  ------| \----          #
 #                                                       |    \`  \  |  p       #
 #    By: jeudy2552 <jeudy2552@floridapoly.edu>          |  \`-\   \ |  o       #
 #                                                       |---\  \   `|  l       #
@@ -17,35 +17,26 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("!")
+f = open('Token.txt', 'r')
+TOKEN = f.read().rstrip()
 
-TOKEN = 'Token'
+bot = Bot(command_prefix = BOT_PREFIX, description='A bot that does a whole host of things that Jeremy works on in his free time.')
 
-client = Bot(command_prefix = BOT_PREFIX)
-
-@client.event
-async def on_message(message):
-# we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-        
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
 
-@client.command(name='Strawpoll',
-                description="Creates a poll for users to vote on",
-                brief="Exercise democracy.",
-                aliases=['strawpoll', 'StrawPoll', 'straw-poll', 'Straw-poll', 'Straw-Poll'],
-                pass_context=True)
-async def strawpoll(context):
-#This shit costs money. I'm out.
+@bot.command()
+async def greet(ctx):
+	await ctx.send(":smiley: :wave: Hello, there!")
 
+#@bot.command(name='Strawpoll',
+#		description="Creates a poll for users to vote on",
+#		brief="Exercise democracy.",
+#		aliases=['strawpoll', 'StrawPoll', 'straw-poll', 'Straw-poll', 'Straw-Poll'],
+#		pass_context=True)
 
-client.run(TOKEN)   
+bot.run(TOKEN)
