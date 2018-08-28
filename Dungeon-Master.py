3c534276763a18f96653d7fcdcef5131307f8dcb
Jeremy Eudy <jeremyeudy@gmail.com>
2018-08-28 08:40:16 -0400

    Fixed syntax errors.

--- Dungeon-Master.py
+++ Dungeon-Master.py
@@ -6,7 +6,7 @@
 #    By: jeudy2552 <jeudy2552@floridapoly.edu>          |  \`-\   \ |  o       #
 #                                                       |---\  \   `|  l       #
 #    Created: 2018/05/29 10:00:02 by jeudy2552          | ` .\  \   |  y       #
-#    Updated: 2018/08/28 08:37:29 by jeudy2552          -------------          #
+#    Updated: 2018/08/28 08:40:13 by jeudy2552          -------------          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -130,7 +130,7 @@ async def announce(ctx, *args):
         text = ''.join(textList[back+1:-1])
         channel = ''.join(textList[front:back])
         textChannel = discord.utils.get(client.get_all_channels(), name=channel)
-        if channel == textChannel:
+        if channel == textChannel.name:
             embed = discord.Embed(title="Announcement:", description=text, color=0xeee657)
             await client.send_message(discord.Object(id=channel, embed=embed))
         else:
