74283936f805af9ed2954709df84ad7b1541b266
Jeremy Eudy <jeremyeudy@gmail.com>
2018-09-07 12:22:22 -0400

    Fixed syntax errors.

--- Dungeon-Master.py
+++ Dungeon-Master.py
@@ -6,7 +6,7 @@
 #    By: jeudy2552 <jeudy2552@floridapoly.edu>          |  \`-\   \ |  o       #
 #                                                       |---\  \   `|  l       #
 #    Created: 2018/05/29 10:00:02 by jeudy2552          | ` .\  \   |  y       #
-#    Updated: 2018/09/07 12:19:18 by jeudy2552          -------------          #
+#    Updated: 2018/09/07 12:22:18 by jeudy2552          -------------          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -206,7 +206,7 @@ async def defaultRole(ctx, *args):
 @bot.command()
 @commands.check(check_if_it_is_me)
 async def invite(ctx):
-    inviteLink = await discord.AuditLogAction(max_age = 1800, max_uses = 1)
+    inviteLink = await discord.AuditLogAction.max_age(1800).max_uses(1)
     embed = discord.Embed(color=0xf41af4)
     embed.add_field(name="Discord Invite Link", value = inviteLink)
     embed.set_footer(text="Discord server invite link.")
