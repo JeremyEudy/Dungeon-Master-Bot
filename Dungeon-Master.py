0139e5ce22d373bc72e604ef3f3b8d1529d2a090
Jeremy Eudy <jeremyeudy@gmail.com>
2018-10-11 03:19:23 +0000

    Fixed syntax errors.

--- Dungeon-Master.py
+++ Dungeon-Master.py
@@ -316,8 +316,8 @@ async def eightball(ctx, *args):
 	choice = random.randint(0, len(responses)-1)			#Randomly pick one
 	await ctx.send(ctx.message.author.mention+": "+responses[choice])
 
-@bot.command(name='strawpoll', description="A simple interface for the strawpoll.me API.")
-async def strawpoll(ctx, *args):
+@bot.command(name='strawpoll', description="A simple interface for the strawpoll.me API.", aliases=['strawpoll'])
+async def newStrawpoll(ctx, *args):
 	message = '{}'.format(''.join(args))
 	#Gets the title of the poll
 	first = message.find("{") + 1
