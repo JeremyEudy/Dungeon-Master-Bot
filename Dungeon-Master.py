b237ef4f77ab17f0384e3adbc0883ac3e27c9988
Jeremy Eudy <jeremyeudy@gmail.com>
2019-03-20 06:18:17 +0000

    spaces

--- Dungeon-Master.py
+++ Dungeon-Master.py
@@ -341,8 +341,8 @@ async def r(ctx, *args):
 	rollString = str(roll)
 	try:
                 await ctx.send(ctx.message.author.mention+": `"+rollString+"` = "+str(eval(roll)))
-        except Exception as e:
-                await ctx.send(ctx.message.author.mention+": "+random.choice(british_insults))
+	except Exception as e:
+		await ctx.send(ctx.message.author.mention+": "+random.choice(british_insults))
 
 @bot.command(name='stats', description="Generates a list of DnD character stats (/r 6(6d4kh3)).", aliases=['rollstats', 'rstats'])
 async def stats(ctx):
